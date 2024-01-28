c35ecb2d897e94e856db635c5e2714baea1fc3e2
Yubin Zou <yubinzou@Yubins-iMac.local>
2024-01-28 14:58:25 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './Components/MovieCard';
+
+
+const API_URL = 'http://www.omdbapi.com/?apikey=6498fed0';
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={()=> searchMovie(searchTerm)}
+        />
+      </div>
+        {movies.length > 0 ?(
+          <div className="Container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.name} movie={movie}/>
+          ))}
+          </div>
+        ):(
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>)}
+      
+    </div>
+  )
+}
+
+export default App
